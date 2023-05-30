d0a1cd224246c33bf1dde518b3e8d839805ffd75
Pattapee Pimton <lordhomeless77@gmail.com>
2023-05-30 10:11:13 +0700

    parseCookies

--- server/api/auth/refresh.get.js
+++ server/api/auth/refresh.get.js
@@ -1,46 +1,50 @@
-import { getRefreshToken } from "~~/server/db/refreshTokens";
-import { getUserById } from "../../db/users";
-import { decodeRefreshToken, generateTokens } from "../../utils/jwt";
+import { sendError } from "h3";
+import { getRefreshTokenByToken } from "../../db/refreshTokens.js";
+import { decodeRefreshToken, generateTokens } from "../../utils/jwt.js";
+import { getUserById } from "../../db/users.js";
 
-export default defineEventHandler(async (e) => {
-  const { refresh_token } = parseCookies(e);
+export default defineEventHandler(async (event) => {
+  const cookies = parseCookies(event);
 
-  if (!refresh_token) {
+  const refreshToken = cookies.refresh_token;
+
+  if (!refreshToken) {
     return sendError(
-      e,
-      createError({ statusCode: 401, statusMessage: "Invalid refresh token." })
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "Refresh token is invalid",
+      })
     );
   }
 
-  await getRefreshToken(refresh_token).catch(() =>
-    sendError(
-      e,
-      createError({ statusCode: 401, statusMessage: "Invalid refresh token." })
-    )
-  );
+  const rToken = await getRefreshTokenByToken(refreshToken);
 
-  const token = await decodeRefreshToken(refresh_token).catch(() =>
-    sendError(
-      e,
-      createError({ statusCode: 401, statusMessage: "Invalid refresh token." })
-    )
-  );
+  if (!rToken) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 401,
+        statusMessage: "Refresh token is invalid",
+      })
+    );
+  }
+
+  const token = decodeRefreshToken(refreshToken);
 
   try {
-    const user = await getUserById(token?.userId).catch(() =>
-      sendError(
-        e,
-        createError({ statusCode: 404, statusMessage: "User not found." })
-      )
-    );
+    const user = await getUserById(token.userId);
 
-    const { accessToken } = await generateTokens(user);
+    const { accessToken } = generateTokens(user);
 
     return { access_token: accessToken };
   } catch (error) {
     return sendError(
-      e,
-      createError({ statusCode: 500, statusMessage: error.message })
+      event,
+      createError({
+        statusCode: 500,
+        statusMessage: "Something went wrong",
+      })
     );
   }
 });
